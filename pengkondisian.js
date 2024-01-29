104452cd87eed243086d4e88c8ca0d9ce5fdf9ce
Malka <malka31ataya@gmail.com>
2024-01-29 14:07:49 +0700

    Pengkondisian

---
+++ pengkondisian.js
@@ -0,0 +1,10 @@
+var x = prompt("Masukan Angka :");
+// alert("Selamat Datang " + nama);
+
+if (x % 2 == 0) {
+    alert(x + " Bilangan Genap");
+} else if(x % 2 == 1) {
+    alert(x + " Bilangan Ganjil");
+} else {
+    alert("Data Bukan Angka");
+}
