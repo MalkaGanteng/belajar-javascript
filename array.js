25041b96ae02f9e8b5e31f595ac910ba8c896377
Malka <malka31ataya@gmail.com>
2024-01-29 13:35:26 +0700

    array

---
+++ array.js
@@ -0,0 +1,8 @@
+// var namarray = new Array()
+var lemari = new Array("Dompet", 50000, true);
+console.log(lemari[1]);
+console.log(lemari[2]);
+
+lemari[3] = "ktp";
+lemari.push("ATM");
+console.log(lemari);
--- fungsi.js
+++ fungsi.js
@@ -1,13 +1,17 @@
 var x = "25";
 var y = "3.14";
 
-var toNum = perseInt(x);
+var toNum = parseInt(x);
 var toFloat = parseFloat(y);
 var toString = toNum.toString();
 console.log(x + toNum);
-
+// Fungsi Deklarasi
 function jumlah(a, b) {
     return a + b;
 }
-
+// Fungsi expresi
+var kurang = function (a, b) {
+    return a - b;
+};
 alert(jumlah(5, 20));
+alert(jumlah(25, 5));
