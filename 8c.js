d9e34be22b4824f5dd1a8af4a2078de80ecc45ab
Malka <malka31ataya@gmail.com>
2024-01-30 07:46:32 +0700

    Latihan 8c

---
+++ 8c.js
@@ -0,0 +1,31 @@
+var pilihanUser = prompt("Apakah kamu memilih Gajah, Orang, atau Semut?");
+var pilihanComputer = Math.random();
+
+if (pilihanComputer < 0.34) {
+    pilihanComputer = "Gajah";
+} else if (pilihanComputer <= 0.67) {
+    pilihanComputer = "Orang";
+} else {
+    pilihanComputer = "Semut";
+}
+
+function bandingkanPilihan(user, computer) {
+    if (user === computer) {
+        return "Hasilnya seri";
+    } else if (
+        (user === "Gajah" && computer === "Orang") ||
+        (user === "Orang" && computer === "Semut") ||
+        (user === "Semut" && computer === "Gajah")
+    ) {
+        return "Kamu menang!";
+    } else {
+        return "Komputer menang!";
+    }
+}
+
+if (pilihanUser === "Gajah" || pilihanUser === "Orang" || pilihanUser === "Semut") {
+    var hasil = bandingkanPilihan(pilihanUser, pilihanComputer);
+    alert("Kamu memilih: " + pilihanUser + "\nKomputer memilih: " + pilihanComputer + "\n\n" + hasil);
+} else {
+    alert("Tidak Ada Di Daftar Pilihan");
+}
